a79a64730d71fb076d289390bf46a26ff750c7a6
Hrishabh-vani <104153833+Hrishabh-vani@users.noreply.github.com>
2022-05-08 15:40:48 +0530

    accounts update

--- Ecom/Ecom/settings.py
+++ Ecom/Ecom/settings.py
@@ -119,6 +119,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+#AUTH USER MODEL
+AUTH_USER_MODEL='accounts.ACCOUNT'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
