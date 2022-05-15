72b954f22b968b039e2c316a239bc1565ae3fcd2
Hrishabh-vani <104153833+Hrishabh-vani@users.noreply.github.com>
2022-05-15 12:14:24 +0530

    project update

--- Ecom/Ecom/settings.py
+++ Ecom/Ecom/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'category',
     'home',
     
-    
+    #Third Party Packages
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -130,6 +131,10 @@ STATIC_ROOT = [BASE_DIR / "static"]
 STATICFILES_DIRS = [] 
 print(STATIC_ROOT)
 print("BASE:", BASE_DIR)
+
+#Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- Ecom/category/models.py
+++ Ecom/category/models.py
@@ -1,14 +1,19 @@
 from enum import unique
+from unicodedata import category
 from django.db import models
 from django.forms import CharField, ImageField
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    slug = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
     description= models.CharField(max_length=250, blank=True)
     image = models.ImageField(upload_to='image/categories', blank=True)
 
+    class mete:
+        db_table = "category"
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
     
     def __str__(self):
         return self.name
